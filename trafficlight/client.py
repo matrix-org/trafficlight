07fdfc527256943be0bc54d5c355c25d7cc8899e
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2022-08-01 16:25:47 +0100

    Add licence headers

--- trafficlight/client.py
+++ trafficlight/client.py
@@ -1,4 +1,17 @@
 # vim: expandtab ts=4:
+# Copyright 2022 The Matrix.org Foundation C.I.C.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import functools
 import logging
 from trafficlight.store import Client, generate_model, get_tests, get_clients, add_client
@@ -32,8 +45,11 @@ def register(uuid):
     
     existing = get_client(uuid)
     if existing is not None:
-        raise Error("Client already exists")
-    
+        if existing.model is not None:
+           raise Error("Client already in use")
+        else:
+           logger.info("Client re-registered, returning OK again if registration matches")
+           return {}
     client = Client(uuid, registration)
     add_client(client)
 
--- trafficlight/status.py
+++ trafficlight/status.py
@@ -1,4 +1,17 @@
 # vim: expandtab ts=4:
+# Copyright 2022 The Matrix.org Foundation C.I.C.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import functools
 import logging
 import io
@@ -57,9 +70,10 @@ def test_image(uuid):
 
 @bp.route("/<string:uuid>/status", methods=["GET"])
 def test_status(uuid):
+    refresh = request.args.get("refresh", default=0, type=int)
     logger.info("Finding test %s", uuid)
     test = get_test(uuid)
     if test is not None:
-       return render_template("status_model.j2.html", test = test) 
+       return render_template("status_model.j2.html", test = test, refresh = refresh) 
     else:
        abort(404)
