f456b8a86ef23502fea5c53c5e953c1651a40f66
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2022-08-12 16:46:31 +0100

    black

--- trafficlight/status.py
+++ trafficlight/status.py
@@ -20,6 +20,7 @@ from flask import Blueprint, abort, render_template, request, send_file, typing
 
 from trafficlight.store import get_clients, get_test, get_tests
 from typing import Any
+
 logging.basicConfig(level=logging.DEBUG)
 # Set transitions' log level to INFO; DEBUG messages will be omitted
 
