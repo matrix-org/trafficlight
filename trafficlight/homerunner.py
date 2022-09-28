4ef88b26b283f84e1a96f6a3c9488709155d1aee
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2022-09-28 11:30:00 +0100

    Handle 0.0.0.0 IP as a valid one.

--- trafficlight/homerunner.py
+++ trafficlight/homerunner.py
@@ -82,6 +82,9 @@ class HomerunnerClient(object):
                     base_image_uri = homeserver["BaseImageURI"]
                     # from response
                     cs_api = response[name]["BaseURL"]
+                    # This replaces 0.0.0.0 with a name that should point there
+                    # Horrid hack, pls remove.
+                    cs_api = cs_api.replace("0.0.0.0", "homeservers")
                     homeserver_configs.append(
                         HomeserverConfig(name, cs_api, base_image_uri)
                     )
