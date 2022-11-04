416170876fcb32259bec3d08f270539c01a40387
RMidhunSuresh <hi@midhun.dev>
2022-11-04 14:40:27 +0530

    WIP

--- trafficlight/__init__.py
+++ trafficlight/__init__.py
@@ -44,7 +44,7 @@ def create_app(test_config: Optional[Dict[str, Any]] = None) -> Quart:
     # Defaults here:
     app.config.update(
         {
-            "TEST_PATTERN": "/**/*_testsuite.py",
+            "TEST_PATTERN": "/**/device_requerying_after_federation_testsuite.py",
             "SERVER_TYPES": "Synapse",
             "CLIENT_TYPES": "ElementWeb",
             "UPLOAD_FOLDER": "/tmp/",
