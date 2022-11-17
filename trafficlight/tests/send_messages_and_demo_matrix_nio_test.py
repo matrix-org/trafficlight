5427c75c4cdab0a13dbe97fe5e729bdc6c2128c5
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2022-11-17 14:29:50 +0000

    Demonstrate doing the register outside of the logic.

--- trafficlight/tests/send_messages_and_demo_matrix_nio_test.py
+++ trafficlight/tests/send_messages_and_demo_matrix_nio_test.py
@@ -15,26 +15,27 @@ class SendMessagesTest(Test):
     def __init__(self) -> None:
         super().__init__()
         self._client_under_test([ElementWeb()], "client_one")
+
         self._server_under_test(Synapse(), ["server"])
 
     async def run(self, client_one: MatrixClient, server: HomeServer) -> None:
-        await client_one.register(server)
-        await client_one.create_room("little test room")
-        await client_one.send_message("hi there!")
-
-        # Not sure if we'd actually want to do this as it stands, and the test isn't that useful
-        # but it demonstrates use of out of band client usage and asserts
-
-        # Once we actually use this in anger, I'd suggest we do some refactoring and provide some utilities
-        # or possibly even make something pre-made and available in the run method parameters.
-
-        # Problem for the person writing that test.
-
         nio_client = AsyncClient(
             server.cs_api, f"@{client_one.localpart}:{server.server_name}"
         )
         try:
-            logger.info("Investigating rooms directly")
+            await nio_client.register(client_one.localpart, client_one.password)
+            await client_one.login(server)
+            await client_one.create_room("little test room")
+            await client_one.send_message("hi there!")
+
+            # Not sure if we'd actually want to do the following as it stands, and the test isn't that useful
+            # but it demonstrates use of out of band client usage and asserts
+
+            # Once we actually use this in anger, I'd suggest we do some refactoring and provide some utilities
+            # or possibly even make something pre-made and available in the run method parameters.
+
+            # Problem for the person writing that test.
+
             await nio_client.login(client_one.password)
             await nio_client.sync()
 
